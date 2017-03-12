af9578eac24ec943f60d06950cb3dbd443934538
Emizaquel <emizaquel@gmail.com>
2017-03-12 10:56:46 +0000

    Good Enough, I need Some Sleep

--- Timetable-U.php
+++ Timetable-U.php
@@ -25,46 +25,6 @@
   </div>
   <div id="page-body">
     <?php
-    $dbserver = "127.0.0.1:51097";
-    $dbuser = "azure";
-    $dbpass = "6#vWHD_$";
-    $dbname = "localdb";
-
-    $conn = mysql_connect($dbserver, $dbuser, $dbpass, $dbname);
-
-    if(! $conn ) {
-      die('Could not connect: ' . mysql_error());
-    }
-
-    if(!isset($_COOKIE["UserID"])) {
-      echo "Is This it?";
-    } else {
-        $UID = $_COOKIE["UserID"];
-
-        $sql = ("SELECT type FROM userdata WHERE usernumber = '" . $UID . "';");
-        mysql_select_db("conferdata");
-        $retval = mysql_query( $sql, $conn );
-
-        if($retval ) {
-          $query = mysql_fetch_row($retval);
-          $userID = $query[0];
-
-          if($userID == 1){
-            header("Location:Timetable-U.php");
-          }else if ($userID == 2){
-            header("Location:Timetable-S.php");
-          }else if ($userID == 3){
-            header("Location:Timetable-St.php");
-          }else if ($userID == 4){
-            header("Location:Timetable-A.php");
-          }else{
-            header("Location:login.php");
-
-          }
-        }
-    }
-    ?>
-    <?php
     $email = $_POST['email'];
     $password = $_POST['pass'];
     $dbserver = "127.0.0.1:51097";
