869ee687fe549be535f2984ea9216fef26a8eb8b
Emizaquel <emizaquel@gmail.com>
2017-03-13 18:16:09 +0000

    testing

--- Event-St.php
+++ Event-St.php
@@ -26,18 +26,20 @@
           if(isset($_GET["EventID"])){
             $EventID = $_GET["EventID"];
             $sql = ("SELECT eventname FROM eventdata WHERE eventnumber = " . $EventID . ";");
+            mysql_select_db("conferdata");
             $retval = mysql_query( $sql, $conn );
+
             if($retval) {
-              echo $retval;
-              // $query = mysql_fetch_row($retval);
-              //
-              // $EName = $query[0];
-              //
-              // if($EName){
-              //   echo $EName;
-              // }else{
-              //   echo "!!Error : name not found!!";
+              $query = mysql_fetch_row($retval);
+
+              $EName = $query[0];
+
+              if($userID){
+                echo $EName;
+              }else{
+                echo "User Details Not Valid";
               }
+            }else{
             }
           }
           ?>
