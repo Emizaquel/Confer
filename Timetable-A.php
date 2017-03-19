dc457d94cb2b034b774362cb5d0d0107a3a830ce
Emizaquel <emizaquel@gmail.com>
2017-03-19 17:43:49 +0000

    Let see if this looks good.

--- Timetable-A.php
+++ Timetable-A.php
@@ -111,80 +111,82 @@
       }
     ?>
     <span id="NewEvent" style="display: none">
-      <form method="POST" action="">
-        <input type="text" placeholder="Enter Name" name="name" style="height: 45px;width: 100%;font-size: 35px;margin: 5px;"><br>
-        <input type="text" placeholder="Enter Description" name="description" style="height: 135px;width: 100%;font-size: 35px;margin: 5px;"><br>
-        <input type="text" placeholder="location" name="location" style="height: 45px;width: 100%;font-size: 35px;margin: 5px;"><br>
-        <input type="datetime-local" name="datetime" style="height: 45px;width: 100%;font-size: 35px;margin: 5px;"><br>
-        <?php
-        $dbserver = "127.0.0.1:51097";
-        $dbuser = "azure";
-        $dbpass = "6#vWHD_$";
-        $dbname = "localdb";
+      <div id='DateHeader'>
+        <form method="POST" action="">
+          <input type="text" placeholder="Enter Name" name="name" style="height: 45px;width: 100%;font-size: 35px;margin: 5px;"><br>
+          <input type="text" placeholder="Enter Description" name="description" style="height: 135px;width: 100%;font-size: 35px;margin: 5px;"><br>
+          <input type="text" placeholder="location" name="location" style="height: 45px;width: 100%;font-size: 35px;margin: 5px;"><br>
+          <input type="datetime-local" name="datetime" style="height: 45px;width: 100%;font-size: 35px;margin: 5px;"><br>
+          <?php
+          $dbserver = "127.0.0.1:51097";
+          $dbuser = "azure";
+          $dbpass = "6#vWHD_$";
+          $dbname = "localdb";
 
-        $conn = mysql_connect($dbserver, $dbuser, $dbpass, $dbname);
-
-        if(! $conn ) {
-          die('Could not connect: ' . mysql_error());
-        }
-
-          $sql = ("SELECT usernumber,name FROM `userdata` WHERE type = 2;");
-          mysql_select_db("conferdata");
-          $retval = mysql_query( $sql, $conn );
-
-          while($row = mysql_fetch_array($retval))
-          {
-             $UserID = $row['usernumber'];
-             $Name = $row['name'];
-
-             echo " <input type=\"radio\" name=\"speaker\" value=\"";
-             echo $UserID;
-             echo "\">";
-             echo $Name;
-             echo "<br>";
-          }
-        ?>
-        <input type = "submit" name = "sub" value = "Login" style="height: 45px;width: 100%;font-size: 35px;margin: 5px;" onclick="document.getElementById('login_text'.style.display=''">
-        <br>
-        <br>
-
-        <?php
-        $dbserver = "127.0.0.1:51097";
-        $dbuser = "azure";
-        $dbpass = "6#vWHD_$";
-        $dbname = "localdb";
-        $email = addslashes($_POST['email']);
-        $password = addslashes(_POST['pass']);
-
-        $conn = mysql_connect($dbserver, $dbuser, $dbpass, $dbname);
-
-        if(! $conn ) {
-          die('Could not connect: ' . mysql_error());
-        }
-
-        if( isset($_POST["sub"]) ){
           $conn = mysql_connect($dbserver, $dbuser, $dbpass, $dbname);
 
-          $sql = ("INSERT INTO `eventdata` (`eventnumber`, `eventname`, `description`, `eventtime`, `speaker`, `location`) VALUES (NULL, 'Get Rid Of this, It\'s a temp', 'Temp Desc', '2017-03-19 23:30:59.000000', '3', 'test location');");
-          mysql_select_db("conferdata");
-          $retval = mysql_query( $sql, $conn );
-
-          if($retval) {
-            $query = mysql_fetch_row($retval);
-
-            $userID = $query[0];
-
-            if($userID){
-              setcookie("UserID", $userID, time() + (86400 * 30), "/");
-              header("Location:Home.php");
-            }else{
-               echo 'User details not valid';
-            }
-          }else{
+          if(! $conn ) {
+            die('Could not connect: ' . mysql_error());
           }
-        }
-        ?>
-      </form>
+
+            $sql = ("SELECT usernumber,name FROM `userdata` WHERE type = 2;");
+            mysql_select_db("conferdata");
+            $retval = mysql_query( $sql, $conn );
+
+            while($row = mysql_fetch_array($retval))
+            {
+               $UserID = $row['usernumber'];
+               $Name = $row['name'];
+
+               echo " <input type=\"radio\" name=\"speaker\" value=\"";
+               echo $UserID;
+               echo "\">";
+               echo $Name;
+               echo "<br>";
+            }
+          ?>
+          <input type = "submit" name = "sub" value = "Login" style="height: 45px;width: 100%;font-size: 35px;margin: 5px;" onclick="document.getElementById('login_text'.style.display=''">
+          <br>
+          <br>
+
+          <?php
+          $dbserver = "127.0.0.1:51097";
+          $dbuser = "azure";
+          $dbpass = "6#vWHD_$";
+          $dbname = "localdb";
+          $email = addslashes($_POST['email']);
+          $password = addslashes(_POST['pass']);
+
+          $conn = mysql_connect($dbserver, $dbuser, $dbpass, $dbname);
+
+          if(! $conn ) {
+            die('Could not connect: ' . mysql_error());
+          }
+
+          if( isset($_POST["sub"]) ){
+            $conn = mysql_connect($dbserver, $dbuser, $dbpass, $dbname);
+
+            $sql = ("INSERT INTO `eventdata` (`eventnumber`, `eventname`, `description`, `eventtime`, `speaker`, `location`) VALUES (NULL, 'Get Rid Of this, It\'s a temp', 'Temp Desc', '2017-03-19 23:30:59.000000', '3', 'test location');");
+            mysql_select_db("conferdata");
+            $retval = mysql_query( $sql, $conn );
+
+            if($retval) {
+              $query = mysql_fetch_row($retval);
+
+              $userID = $query[0];
+
+              if($userID){
+                setcookie("UserID", $userID, time() + (86400 * 30), "/");
+                header("Location:Home.php");
+              }else{
+                 echo 'User details not valid';
+              }
+            }else{
+            }
+          }
+          ?>
+        </form>
+      </div>
       <br><a onclick="document.getElementById('NewEvent').style.display='none';" class="link">[Hide]</a>
     </span>
     <br><a onclick="document.getElementById('NewEvent').style.display='';" class="link">[New Event]</a>
