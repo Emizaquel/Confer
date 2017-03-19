d89a476279e76d898215e4ff73194b6608668fd1
Emizaquel <emizaquel@gmail.com>
2017-03-19 22:06:56 +0000

    Add User

--- Admin-A.php
+++ Admin-A.php
@@ -28,39 +28,13 @@
     <span id="NewEvent" style="display: block;background-color: white;border-radius: 15px;border-style: solid;border-color: grey;border-width: 10px;color: black;overflow-x: hidden;min-height: 72px;padding: 10px;padding-top: 45px;display: none;">
       <form method="POST" action="">
         <input type="text" placeholder="Enter Name" name="name" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;"><br>
-        <textarea name="description" style="height: 135px;width: 98%;font-size: 35px;margin: 5px;">Enter Description here</textarea><br>
-        <input type="text" placeholder="location" name="location" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;"><br>
-        <input type="datetime-local" name="datetime" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;"><br>
-        <br>Speaker<br>
-        <?php
-        $dbserver = "127.0.0.1:51097";
-        $dbuser = "azure";
-        $dbpass = "6#vWHD_$";
-        $dbname = "localdb";
-
-        $conn = mysql_connect($dbserver, $dbuser, $dbpass, $dbname);
-
-        if(! $conn ) {
-          die('Could not connect: ' . mysql_error());
-        }
-
-          $sql = ("SELECT usernumber,name FROM `userdata` WHERE type = 2;");
-          mysql_select_db("conferdata");
-          $retval = mysql_query( $sql, $conn );
-
-          while($row = mysql_fetch_array($retval))
-          {
-             $UserID = $row['usernumber'];
-             $Name = $row['name'];
-
-             echo " <input type=\"radio\" name=\"speaker\" style=\"width:2em;height:2em;\" value=\"";
-             echo $UserID;
-             echo "\">  ";
-             echo $Name;
-             echo "<br>";
-          }
-        ?>
-        <br>
+        <input type="text" placeholder="Enter E-Mail" name="email" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;"><br>
+        <input type="password" placeholder="password" name="password" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;"><br>
+        <br>type<br>
+        <input type="radio" name="type" style="width:2em;height:2em;" value="1">User<br>
+        <input type="radio" name="type" style="width:2em;height:2em;" value="2">Speaker<br>
+        <input type="radio" name="type" style="width:2em;height:2em;" value="3">Staff<br>
+        <input type="radio" name="type" style="width:2em;height:2em;" value="4">Admin<br>
         <input type = "submit" name = "sub" value = "Submit" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;" onclick="document.getElementById('login_text'.style.display=''">
 
         <?php
@@ -68,11 +42,10 @@
         $dbuser = "azure";
         $dbpass = "6#vWHD_$";
         $dbname = "localdb";
-        $eventname = addslashes($_POST['name']);
-        $description = addslashes($_POST['description']);
-        $eventtime = addslashes($_POST['datetime']);
-        $speaker = $_POST['speaker'];
-        $location = addslashes($_POST['location']);
+        $name = addslashes($_POST['name']);
+        $email = addslashes($_POST['email']);
+        $password = addslashes($_POST['password']);
+        $type = $_POST['type'];
 
         $conn = mysql_connect($dbserver, $dbuser, $dbpass, $dbname);
 
@@ -83,7 +56,7 @@
         if( isset($_POST["sub"]) ){
           $conn = mysql_connect($dbserver, $dbuser, $dbpass, $dbname);
 
-          $sql = ("INSERT INTO `eventdata` (`eventnumber`, `eventname`, `description`, `eventtime`, `speaker`, `location`) VALUES (NULL, \"{$eventname}\", \"{$description}\", \"{$eventtime}\", \"{$speaker}\", \"{$location}\");");
+          $sql = ("INSERT INTO `userdata`(`usernumber`, `email`, `password`, `name`, `type`) VALUES (NULL,'{$email}','{$password}','{$name}','{$type}');");
           mysql_select_db("conferdata");
           $retval = mysql_query( $sql, $conn );
 
