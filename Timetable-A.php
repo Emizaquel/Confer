af3587072000de7cced99717caff4449c3df3349
Emizaquel <emizaquel@gmail.com>
2017-03-19 18:32:04 +0000

    Lets See if this works

--- Timetable-A.php
+++ Timetable-A.php
@@ -113,7 +113,7 @@
     <span id="NewEvent" style="display: block;background-color: white;border-radius: 15px;border-style: solid;border-color: grey;border-width: 10px;color: black;overflow-x: hidden;min-height: 72px;padding: 10px;display: none;">
         <form method="POST" action="">
           <input type="text" placeholder="Enter Name" name="name" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;"><br>
-          <input type="text" placeholder="Enter Description" name="description" style="height: 135px;width: 98%;font-size: 35px;margin: 5px;"><br>
+          <textarea name="description" style="height: 135px;width: 98%;font-size: 35px;margin: 5px;">Enter Description here</textarea><br>
           <input type="text" placeholder="location" name="location" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;"><br>
           <input type="datetime-local" name="datetime" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;"><br>
           <br>Speaker<br>
