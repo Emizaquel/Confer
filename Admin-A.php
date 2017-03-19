bc64149c050470f8c9a1b58b59c8a59f00c6de7a
Emizaquel <emizaquel@gmail.com>
2017-03-19 19:46:21 +0000

    tweaks ahoy

--- Admin-A.php
+++ Admin-A.php
@@ -25,7 +25,145 @@
     </div>
   </div>
   <div id="page-body">
-    Insert PHP table of users here.
-    <br><!-- This is for readability on a computer, don't get rid of it. -->
+    <span id="NewEvent" style="display: block;background-color: white;border-radius: 15px;border-style: solid;border-color: grey;border-width: 10px;color: black;overflow-x: hidden;min-height: 72px;padding: 10px;padding-top: 45px;display: none;">
+      <form method="POST" action="">
+        <input type="text" placeholder="Enter Name" name="name" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;"><br>
+        <textarea name="description" style="height: 135px;width: 98%;font-size: 35px;margin: 5px;">Enter Description here</textarea><br>
+        <input type="text" placeholder="location" name="location" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;"><br>
+        <input type="datetime-local" name="datetime" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;"><br>
+        <br>Speaker<br>
+        <?php
+        $dbserver = "127.0.0.1:51097";
+        $dbuser = "azure";
+        $dbpass = "6#vWHD_$";
+        $dbname = "localdb";
+
+        $conn = mysql_connect($dbserver, $dbuser, $dbpass, $dbname);
+
+        if(! $conn ) {
+          die('Could not connect: ' . mysql_error());
+        }
+
+          $sql = ("SELECT usernumber,name FROM `userdata` WHERE type = 2;");
+          mysql_select_db("conferdata");
+          $retval = mysql_query( $sql, $conn );
+
+          while($row = mysql_fetch_array($retval))
+          {
+             $UserID = $row['usernumber'];
+             $Name = $row['name'];
+
+             echo " <input type=\"radio\" name=\"speaker\" style=\"width:2em;height:2em;\" value=\"";
+             echo $UserID;
+             echo "\">  ";
+             echo $Name;
+             echo "<br>";
+          }
+        ?>
+        <br>
+        <input type = "submit" name = "sub" value = "Submit" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;" onclick="document.getElementById('login_text'.style.display=''">
+
+        <?php
+        $dbserver = "127.0.0.1:51097";
+        $dbuser = "azure";
+        $dbpass = "6#vWHD_$";
+        $dbname = "localdb";
+        $eventname = addslashes($_POST['name']);
+        $description = addslashes($_POST['description']);
+        $eventtime = addslashes($_POST['datetime']);
+        $speaker = $_POST['speaker'];
+        $location = addslashes($_POST['location']);
+
+        $conn = mysql_connect($dbserver, $dbuser, $dbpass, $dbname);
+
+        if(! $conn ) {
+          die('Could not connect: ' . mysql_error());
+        }
+
+        if( isset($_POST["sub"]) ){
+          $conn = mysql_connect($dbserver, $dbuser, $dbpass, $dbname);
+
+          $sql = ("INSERT INTO `eventdata` (`eventnumber`, `eventname`, `description`, `eventtime`, `speaker`, `location`) VALUES (NULL, \"{$eventname}\", \"{$description}\", \"{$eventtime}\", \"{$speaker}\", \"{$location}\");");
+          mysql_select_db("conferdata");
+          $retval = mysql_query( $sql, $conn );
+
+          if($retval) {
+            $query = mysql_fetch_row($retval);
+
+            $userID = $query[0];
+
+            if($userID){
+              setcookie("UserID", $userID, time() + (86400 * 30), "/");
+              header("Location:Home.php");
+            }else{
+               echo 'User details not valid';
+            }
+          }else{
+          }
+        }
+        ?>
+        <a onclick="document.getElementById('NewEventButton').style.display='block'; document.getElementById('NewEvent').style.display='none';" class="link"><button type="button" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;border-radius: 0;">Cancel</button></a>
+      </form>
+    </span>
+    <a onclick="document.getElementById('NewEvent').style.display='block'; document.getElementById('NewEventButton').style.display='none';" class="link">
+      <span id="NewEventButton" style="display: block;background-color: white;border-radius: 15px;border-style: solid;border-color: grey;border-width: 10px;color: black;overflow-x: hidden;min-height: 36px;padding: 10px;text-align:center;">
+        New Event
+      </span>
+    </a>
+    <br>
+    <?php
+    $dbserver = "127.0.0.1:51097";
+    $dbuser = "azure";
+    $dbpass = "6#vWHD_$";
+    $dbname = "localdb";
+
+    $conn = mysql_connect($dbserver, $dbuser, $dbpass, $dbname);
+
+    if(! $conn ) {
+      die('Could not connect: ' . mysql_error());
+    }
+
+      $sql = ("SELECT usernumber,name FROM `userdata` ORDER BY `userdata`.`usernumber` ASC");
+      mysql_select_db("conferdata");
+      $retval = mysql_query( $sql, $conn );
+
+      while($row = mysql_fetch_array($retval))
+      {
+         $UserID = $row['eventnumber'];
+         $UserName = $row['eventname'];
+
+         echo "<a id='EventListing' href = 'User.php?UserID={$UserID}'>{$UserName}<br></a>";
+      }
+      echo "<div id='EventBottom'></div>";
+      echo "</div></div><br>";
+
+      if(!isset($_COOKIE["UserID"])) {
+        header("Location:login.php");
+      } else {
+          $UID = $_COOKIE["UserID"];
+
+          $sql = ("SELECT type FROM userdata WHERE usernumber = '" . $UID . "';");
+          mysql_select_db("conferdata");
+          $retval = mysql_query( $sql, $conn );
+
+          if($retval ) {
+            $query = mysql_fetch_row($retval);
+            $userID = $query[0];
+
+            if($userID == 1){
+              header("Location:Home-U.php");
+            }else if ($userID == 2){
+              header("Location:Home-S.php");
+            }else if ($userID == 3){
+              header("Location:Home-St.php");
+            }else if ($userID == 4){
+            }else{
+              header("Location:login.php");
+
+            }
+          }
+      }
+    ?>
+    <br><br><!-- This is for readability on a computer, don't get rid of it. -->
   </div>
 </div>
