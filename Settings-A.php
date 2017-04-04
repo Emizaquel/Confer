9d3074515261c16fc2412f78dcc8ebfdc9073b09
Emizaquel <emizaquel@gmail.com>
2017-04-04 13:39:13 +0800

    Title fix testing #6

--- Settings-A.php
+++ Settings-A.php
@@ -91,7 +91,7 @@
       <br><a onclick="document.getElementById('EditDetails').style.display=''; document.getElementById('UserDetails').style.display='none';" class="link"><button type="button" id="customButton1">Edit Details</button></a><br>
       <br><a href="logout.php"><button type="button" id="customButton1">logout</button></a>
 
-      <script>test();</script>
+      <script>autoSizeText();</script>
     </span>
     <span id="EditDetails" style="display: none">
       <form method="POST" action="">
--- scripts.js
+++ scripts.js
@@ -1,7 +1,27 @@
-function test(){
-  alert("HEYYY!");
+function autoSizeText() {
+  alert("HEY!!");
+  var el, elements, i, len, results;
+  elements = $('#title-content');
+  console.log(elements);
+  if (elements.length < 0) {
+    return;
+  }
+  results = [];
+  for (i = 0, len = elements.length; i < len; i++) {
+    el = elements[i];
+    results.push((function(el) {
+      var resizeText, results1;
+      resizeText = function() {
+        var elNewFontSize;
+        elNewFontSize = (parseInt($(el).css('font-size').slice(0, -2)) - 1) + 'px';
+        return $(el).css('font-size', elNewFontSize);
+      };
+      results1 = [];
+      while (el.scrollHeight > el.offsetHeight) {
+        results1.push(resizeText());
+      }
+      return results1;
+    })(el));
+  }
+  return results;
 };
-
-$(document).ready(function() {
-  return test();
-});
