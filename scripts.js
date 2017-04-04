4cddbaa1605ee990b1438f25e39c7f08e4fb8a58
Emizaquel <emizaquel@gmail.com>
2017-04-04 15:26:38 +0800

    Title fix attempt #12

--- scripts.js
+++ scripts.js
@@ -1,29 +1,34 @@
 var autoSizeText;
-
 autoSizeText = function() {
-  alert("HEY!!");
-  var el, elements, i, len, results;
-  elements = $('#title-content');
+  alert("HEY!!!");
+  var el, elements, _i, _len, _results;
+  elements = $('.resize');
   console.log(elements);
   if (elements.length < 0) {
     return;
   }
-  results = [];
-  for (i = 0, len = elements.length; i < len; i++) {
-    el = elements[i];
-    results.push((function(el) {
-      var resizeText, results1;
+  _results = [];
+  for (_i = 0, _len = elements.length; _i < _len; _i++) {
+    el = elements[_i];
+    _results.push((function(el) {
+      var resizeText, _results1;
       resizeText = function() {
         var elNewFontSize;
         elNewFontSize = (parseInt($(el).css('font-size').slice(0, -2)) - 1) + 'px';
         return $(el).css('font-size', elNewFontSize);
       };
-      results1 = [];
+      _results1 = [];
       while (el.scrollHeight > el.offsetHeight) {
-        results1.push(resizeText());
+        _results1.push(resizeText());
       }
-      return results1;
+      return _results1;
     })(el));
   }
-  return results;
+  return _results;
 };
+
+
+
+$(document).ready(function() {
+  return autoSizeText();
+});
