3d8d17635e8c28a29ab312cce6044badc8af20fe
Emizaquel <emizaquel@gmail.com>
2017-04-09 15:17:35 +0800

    Title fix attempt #29

--- scripts.js
+++ scripts.js
@@ -1,25 +1,21 @@
-function greetings(){
-  var contentHeight = $("#content").height();
-  var containerHeight = $("#title-content").height();
-  var el = document.getElementById("title-content");
-  var style = window.getComputedStyle(el, null).getPropertyValue("font-size");
-  var fontSize = parseFloat(style);
-  var newFont = fontSize - 0.1;
-  alert(contentHeight);
-}
+var autoSizeText;
 
-function autoSizeText() {
-  var contentHeight = $("#content").height();
-  var containerHeight = $("#title-content").height();
-  var el = document.getElementById("title-content");
-  var style = window.getComputedStyle(el, null).getPropertyValue("font-size");
+autoSizeText = function() {
+  var contentHeight = $('#content').height();
+  var containerHeight = $('#resize').height();
+  var el = document.getElementById('resize');
+  var style = window.getComputedStyle(el, null).getPropertyValue('font-size');
   var fontSize = parseFloat(style);
-  var newFont = fontSize - 0.1;
+  var newFont = fontSize - 1;
   alert(contentHeight);
   while(contentHeight > containerHeight) {
-  	el.style.fontSize = (newFont) + "px";
-    var newFont = newFont - 0.1;
-    var contentHeight = $("#content").height();
+  	el.style.fontSize = (newFont) + 'px';
+    var newFont = newFont - 1;
+    var contentHeight = $('#content').height();
   }
   alert(contentHeight);
 };
+
+$(document).ready(function() {
+  return autoSizeText();
+});
