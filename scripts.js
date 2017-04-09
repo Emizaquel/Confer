279cd216ddd9632f9d00e43f893ae0e7414035da
Emizaquel <emizaquel@gmail.com>
2017-04-09 15:09:32 +0800

    Title fix attempt #24

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
 function greetings(){
-  alert('hello');
+  alert("hello");
 }
 
 function autoSizeText() {
-  var contentHeight = $('#content').height();
-  var containerHeight = $('#title-content').height();
-  var el = document.getElementById('title-content');
-  var style = window.getComputedStyle(el, null).getPropertyValue('font-size');
+  var contentHeight = $("#content").height();
+  var containerHeight = $("#title-content").height();
+  var el = document.getElementById("title-content");
+  var style = window.getComputedStyle(el, null).getPropertyValue("font-size");
   var fontSize = parseFloat(style);
   var newFont = fontSize - 0.1;
   alert(contentHeight);
   while(contentHeight > containerHeight) {
-  	el.style.fontSize = (newFont) + 'px';
+  	el.style.fontSize = (newFont) + "px";
     var newFont = newFont - 0.1;
-    var contentHeight = $('#content').height();
+    var contentHeight = $("#content").height();
   }
   alert(contentHeight);
 };
