5ddb8c23c7ae43f90266e8c6d6551ddeba38578a
Emizaquel <emizaquel@gmail.com>
2017-04-09 15:15:36 +0800

    Title fix attempt #28

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
 function greetings(){
-  alert("hello");
+  var contentHeight = $("#content").height();
+  var containerHeight = $("#title-content").height();
+  var el = document.getElementById("title-content");
+  var style = window.getComputedStyle(el, null).getPropertyValue("font-size");
+  var fontSize = parseFloat(style);
+  var newFont = fontSize - 0.1;
+  alert(contentHeight);
 }
 
 function autoSizeText() {
