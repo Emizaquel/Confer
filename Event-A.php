936b3b3a36a08ed277782f71ce38eacc06577565
Emizaquel <emizaquel@gmail.com>
2017-04-12 05:40:17 +0800

    Working on reminder system 10

--- Event-A.php
+++ Event-A.php
@@ -83,8 +83,8 @@
     $Description = $query['description'];
     $SID = $query['speaker'];
 
-    // $date = date('Y-m-d', strtotime($EventDateTime));
-    // $time = date('H:i:s', strtotime($EventDateTime));
+    $date = date('Y-m-d', strtotime($EventDateTime));
+    $time = date('H:i:s', strtotime($EventDateTime));
 
     // $LocationArray = split(" ", $LocationSpaces);
     // $Location = join("+", $LocationArray);
@@ -96,8 +96,8 @@
     //
     // $Presenter = $query[0];
 
-    // echo $date;
-    // echo "<br>";
+    echo $date;
+    echo "<br>";
     echo $time;
     echo "<br>";
     echo "<br>";
