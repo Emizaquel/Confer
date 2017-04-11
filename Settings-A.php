a4a94dc78e25fec28ce7d10df3798509bc294371
Emizaquel <emizaquel@gmail.com>
2017-04-11 21:54:49 +0800

    Testing image upload 8

--- Settings-A.php
+++ Settings-A.php
@@ -153,14 +153,14 @@
                 $xstart = ($srcwidth - $srcheight)/2;
                 $xend = $xstart + $srcheight;
                 $im2 = imagecreatetruecolor(500, 500);
-                if(imagecopyresampled ( $im2 , $im , 0 , 0 , $xstart , 0 , 500 , 500 , $xend , $srcheight )){
+                if(imagecopyresampled ( $im2 , $im , 0 , 0 , $xstart , 0 , 500 , 500 , $srcheight , $srcheight )){
                   echo ("success x");
                 }
               } else {
                 $ystart = ($srcheight - $srcwidth)/2;
                 $yend = $ystart + $srcwidth;
                 $im2 = imagecreatetruecolor(500, 500);
-                if(imagecopyresampled ( $im2 , $im , 0 , 0 , 0 , $ystart , 500 , 500 , $srcwidth , $yend )){
+                if(imagecopyresampled ( $im2 , $im , 0 , 0 , 0 , $ystart , 500 , 500 , $srcwidth , $srcwidth )){
                   echo ("success y");
                 }
               }
