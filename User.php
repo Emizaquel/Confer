061c93509eeb15cde9fe2a6715ae67d4547fb0af
Emizaquel <emizaquel@gmail.com>
2017-04-12 03:44:45 +0800

    Working on random password email 8

--- User.php
+++ User.php
@@ -101,11 +101,43 @@
         }
         $message = "Hello {$username},\r\n\r\n You have requested a new password from an administrator for this event. If you have not asked for a password, please contact the staff for this effect and report it. \r\n\r\nYour new password is : {$sendpass} \r\n\r\nWe hope this does not inconvenience you.";
         $message = wordwrap($message, 70, "\r\n");
-        if(mail("Chiragh2355@gmail.com","New Password",$message)){
-          echo("Success!");
-        }else{
-          echo("$sendpass");
-        }
+        echo("$sendpass");
+
+        $url = 'https://api.sendgrid.com/';
+        $user = 'azure_caf314d7239d3e6933a43d5514d85347@azure.com';
+        $pass = 'Chirag25';
+
+        $params = array(
+             'api_user' => $user,
+             'api_key' => $pass,
+             'to' => 'ChiragH2355@gmail.com',
+             'subject' => 'testing from curl',
+             'html' => 'testing body',
+             'text' => 'testing body',
+             'from' => 'do-not-reply@confer-azure.com',
+          );
+
+        $request = $url.'api/mail.send.json';
+
+        // Generate curl request
+        $session = curl_init($request);
+
+        // Tell curl to use HTTP POST
+        curl_setopt ($session, CURLOPT_POST, true);
+
+        // Tell curl that this is the body of the POST
+        curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+
+        // Tell curl not to return headers, but do return the response
+        curl_setopt($session, CURLOPT_HEADER, false);
+        curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+        // obtain response
+        $response = curl_exec($session);
+        curl_close($session);
+
+        // print everything out
+        print_r($response);
       }
       ?>
     </form>
