e47d398e5d53274c9f82932173995794fbdd99e8
Emizaquel <emizaquel@gmail.com>
2017-04-12 12:58:09 +0800

    Working on reminder system 29

--- Event-U.php
+++ Event-U.php
@@ -139,8 +139,7 @@
       if(mysqli_query( $conn ,  $sql)){
         echo "<form method=\"POST\" action=\"\">
           <input type = \"submit\" name = \"remindme\" value = \"Remind Me\" style=\"height: 45px;width: 98%;font-size: 35px;margin: 5px;\">
-        </form>
-        <br>Success!";
+        </form>";
       }
     }else if(isset($_POST["remindme"])){
       $sql = ("INSERT INTO `reminderdata` (`usernumber`, `eventnumber`) VALUES ('{$userID}', '{$EventID}');");
@@ -148,8 +147,7 @@
       if(mysqli_query( $conn ,  $sql)){
         echo "<form method=\"POST\" action=\"\">
           <input type = \"submit\" name = \"forgetme\" value = \"Don't Remind Me\" style=\"height: 45px;width: 98%;font-size: 35px;margin: 5px;\">
-        </form>
-        <br>Success!";
+        </form>";
       }
     }else if($query){
       echo"<form method=\"POST\" action=\"\">
