5218e345e419ba512521d444a2a6ae5cbd613267
Emizaquel <emizaquel@gmail.com>
2017-04-15 15:12:49 +0800

    testing editor 6

--- editortest.php
+++ editortest.php
@@ -1,6 +1,6 @@
 <script src="ckeditor/ckeditor.js"></script>
 <form>
-    <textarea name="editor1" id="editor1" rows="10" cols="80">
+    <textarea name="editor1" rows="10" cols="80">
         This is my textarea to be replaced with CKEditor.
     </textarea>
     <script>
@@ -10,7 +10,8 @@
     </script>
     <input type = "submit" name = "sub" value = "Submit" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;"><br><br>
     <?php
-        $editor_data = $_POST[ 'editor1' ];
+        $editor_data = $_POST["editor1"];
         echo $_POST;
+        echo $editor_data;
     ?>
 </form>
