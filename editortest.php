d73b00319c5e881ee59101ac3ea88ee8312188b8
Emizaquel <emizaquel@gmail.com>
2017-04-15 14:50:30 +0800

    testing editor 3

--- editortest.php
+++ editortest.php
@@ -1,11 +1,10 @@
 <script src="ckeditor/ckeditor.js"></script>
 <form>
-  <textarea class="ckeditor" cols="80" id="editor1" name="editor1" rows="10">
+  <textarea class="ckeditor" cols="80" id="editor1" name="editor1" rows="10"><br><br>
+  <input type = "submit" name = "sub" value = "Submit" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;"><br><br>
   <script>
-    CKEDITOR.replace( 'editor1' );
-  </script><br>
-  <input type = "submit" name = "sub" value = "Submit" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;">
-
+    CKEDITOR.replace('editor1');
+  </script>
   <?php
   if( isset($_POST["sub"]) ){
     $text = addslashes($_POST['editor1']);
