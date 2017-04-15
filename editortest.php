2d3dd5e13f4bff5822769137153d0e003d3113a3
Emizaquel <emizaquel@gmail.com>
2017-04-15 15:43:34 +0800

    testing editor 6

--- editortest.php
+++ editortest.php
@@ -5,20 +5,17 @@
 </head>
 <body>
 	<form method="post">
-		<p>
-			My Editor:<br />
 			<textarea id="editor1" name="editor1">&lt;p&gt;Initial value.&lt;/p&gt;</textarea>
 			<script type="text/javascript">
 				CKEDITOR.replace( 'editor1' );
 			</script>
-		</p>
 		<p>
 			<input type="submit" />
 		</p>
 	</form>
   <?php
   	$editor_data = $_POST[ 'editor1' ];
-    $usrimgpath = $_SERVER['DOCUMENT_ROOT'] . "/hometext.txt";
+    $file = $_SERVER['DOCUMENT_ROOT'] . "/hometext.txt";
     echo $editor_data;
     echo(file_put_contents($file, $editor_data));
   ?>
