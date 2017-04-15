b261b1fb7d334b85f8c021d3c7ae7f447f3a1458
Emizaquel <emizaquel@gmail.com>
2017-04-15 15:18:17 +0800

    testing editor 6

--- editortest.php
+++ editortest.php
@@ -1,16 +1,10 @@
 <script src="ckeditor/ckeditor.js"></script>
 <form>
-    <textarea name="editor1" rows="10" cols="80">
+    <textarea name="editor1" rows="10" cols="80" class="ckeditor">
         This is my textarea to be replaced with CKEditor.
     </textarea>
-    <script>
-        // Replace the <textarea id="editor1"> with a CKEditor
-        // instance, using default configuration.
-        CKEDITOR.replace( 'editor1' );
-    </script>
     <input type = "submit" name = "sub" value = "Submit" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;"><br><br>
     <?php
-        $editor_data = $_POST["editor1"];
         echo $_POST;
         echo $editor_data;
     ?>
