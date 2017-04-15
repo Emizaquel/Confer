18899363a0f829ab2128f218d4cfd841df273cd2
Emizaquel <emizaquel@gmail.com>
2017-04-15 14:56:52 +0800

    testing editor 3

--- editortest.php
+++ editortest.php
@@ -1,5 +1,5 @@
 <form>
-  <textarea class="ckeditor" cols="80" id="editor1" name="editor1" rows="10"><br><br>
+  <textarea class="ckeditor" cols="80" id="editor1" name="editor1" rows="10"></textarea><br><br>
   <input type = "submit" name = "sub" value = "Submit" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;"><br><br>
 </form>
 
