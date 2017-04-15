4108338d9b43addf4402fa37f1f5504525e6707f
Emizaquel <emizaquel@gmail.com>
2017-04-15 14:57:37 +0800

    testing editor 4

--- editortest.php
+++ editortest.php
@@ -1,6 +1,10 @@
+<script src="ckeditor/ckeditor.js"></script>
 <form>
   <textarea class="ckeditor" cols="80" id="editor1" name="editor1" rows="10"></textarea><br><br>
   <input type = "submit" name = "sub" value = "Submit" style="height: 45px;width: 98%;font-size: 35px;margin: 5px;"><br><br>
+  <script>
+    CKEDITOR.replace('editor1');
+  </script>
 </form>
 
 <?php
