c7d14a5472ac2422575e8e6db636f214fc93579a
Emizaquel <emizaquel@gmail.com>
2017-04-15 17:40:29 +0800

    testing editor 8

--- editortest.php
+++ editortest.php
@@ -7,6 +7,11 @@
 	<form method="post">
 			<textarea id="editor1" name="editor1"><?php
       $file = $_SERVER['DOCUMENT_ROOT'] . "/hometext.txt";
+      if(isset($_POST[ 'editor1' ])){
+        $editor_data = $_POST[ 'editor1' ];
+        echo $editor_data;
+        file_put_contents($file, $editor_data);
+      }
       $current = file_get_contents ($file);
       echo $current;
        ?></textarea>
@@ -17,10 +22,5 @@
 			<input type="submit" />
 		</p>
 	</form>
-  <?php
-  	$editor_data = $_POST[ 'editor1' ];
-    echo $editor_data;
-    file_put_contents($file, $editor_data);
-  ?>
 </body>
 </html>
