6942f371015264c6a98cd970ee04e7e797859806
Emizaquel <emizaquel@gmail.com>
2017-04-18 02:35:54 +0800

    Adding the Jobs System 7

--- Event-St.php
+++ Event-St.php
@@ -42,39 +42,6 @@
             }else{
             }
           }
-
-          if(!isset($_COOKIE["UserID"])) {
-            header("Location:login.php");
-          } else {
-              $UID = $_COOKIE["UserID"];
-
-              $sql = ("SELECT type FROM userdata WHERE usernumber = '" . $UID . "';");
-              ((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . conferdata));
-              $retval = mysqli_query( $conn ,  $sql);
-
-              if($retval ) {
-                $query = mysqli_fetch_row($retval);
-                $userID = $query[0];
-
-                if($userID == 1){
-                  header("Location:Event-U.php?EventID={$EventID}");
-                }else if ($userID == 2){
-                  header("Location:Event-S.php?EventID={$EventID}");
-                }else if ($userID == 3){
-                }else if ($userID == 4){
-                  header("Location:Event-A.php?EventID={$EventID}");
-                }else{
-                  header("Location:login.php");
-                }
-              }
-
-              $sql = ("SELECT jobnumber FROM jobdata WHERE usernumber = {$UID} AND jobnumber == 3;");
-              ((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . conferdata));
-              $retval = mysqli_query( $conn ,  $sql);
-              if($retval){
-                $JobValue = TRUE;
-              }
-          }
           ?></div></div>
       </div>
     </div>
@@ -82,17 +49,12 @@
   <div id="footer">
     <div id="nav-bar">
       <div id="image-border"><a href="timetable.php"><img src="home.png" height="100%"></a></div>
-      <div id="image-border"><a href="admin.php"><img src="admin.png" height="100%"></a></div>
       <div id="image-border"><a href="help.php"><img src="help.png" height="100%"></a></div>
       <div id="image-border"><a href="settings.php"><img src="setting.png" height="100%"></a></div>
     </div>
   </div>
   <div id="page-body">
     <?php
-
-    if($JobValue){
-      echo "<span id=\"EditEventButton\">"
-    }
     $dbserver = "127.0.0.1:51097";
     $dbuser = "azure";
     $dbpass = "6#vWHD_$";
@@ -140,13 +102,41 @@
     echo $LocationSpaces;
     echo "</a>";
 
+
+
+    if(!isset($_COOKIE["UserID"])) {
+      header("Location:login.php");
+    } else {
+        $UID = $_COOKIE["UserID"];
+
+        $sql = ("SELECT type FROM userdata WHERE usernumber = '" . $UID . "';");
+        ((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . conferdata));
+        $retval = mysqli_query( $conn ,  $sql);
+
+        if($retval ) {
+          $query = mysqli_fetch_row($retval);
+          $userID = $query[0];
+
+          if($userID == 1){
+            header("Location:Event-U.php?EventID={$EventID}");
+          }else if ($userID == 2){
+            header("Location:Event-S.php?EventID={$EventID}");
+          }else if ($userID == 3){
+          }else if ($userID == 4){
+            header("Location:Event-A.php?EventID={$EventID}");
+          }else{
+            header("Location:login.php");
+          }
+        }
+    }
+
     $sql = ("SELECT * FROM reminderdata WHERE eventnumber = {$EventID} AND usernumber = {$UID};");
     ((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . conferdata));
     $retval = mysqli_query( $conn ,  $sql);
     $query = mysqli_fetch_array($retval);
 
     if(isset($_POST["forgetme"])){
-      $sql = ("DELETE FROM `reminderdata` WHERE usernumber = $UID} AND eventnumber = {$EventID}");
+      $sql = ("DELETE FROM `reminderdata` WHERE usernumber = {$UID} AND eventnumber = {$EventID}");
       ((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . conferdata));
       if(mysqli_query( $conn ,  $sql)){
         echo "<form method=\"POST\" action=\"\">
@@ -169,80 +159,6 @@
       echo"<form method=\"POST\" action=\"\">
         <input type = \"submit\" name = \"remindme\" value = \"Remind Me\" style=\"height: 45px;width: 98%;font-size: 35px;margin: 5px;\">
       </form>";
-    }
-
-        if( isset($_POST["del"]) ){
-          $sql3 = ("DELETE FROM `eventdata` WHERE eventnumber = {$EventID};");
-          ((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . conferdata));
-          mysqli_query( $conn ,  $sql3);
-        }
-
-        if( isset($_POST["sub"]) ){
-          $EditName = addslashes($_POST['name']);
-          $EditDesc = addslashes($_POST['description']);
-          $EditTime = addslashes($_POST['datetime']);
-          $EditSpeaker = addslashes($_POST['speaker']);
-          $EditLocation = addslashes($_POST['location']);
-
-          $sql3 = ("SELECT name FROM userdata WHERE usernumber = {$EditSpeaker};");
-          ((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . conferdata));
-          $retval3 = mysqli_query( $conn ,  $sql3);
-          $editspeakername = mysqli_fetch_array($retval3);
-
-          $editdisplaydate = date('l jS \of F Y h:i:s A', strtotime($EditTime));
-
-          $sql = "UPDATE `eventdata` SET `eventname`=\"{$EditName}\",`description`=\"{$EditDesc}\",`eventtime`=\"{$EditTime}\",`speaker`=$EditSpeaker,`location`=\"$EditLocation\" WHERE `eventdata`.`eventnumber` = {$EventID};";
-          ((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . conferdata));
-          if(mysqli_query( $conn ,  $sql)){
-            $sql2 = ("SELECT usernumber FROM reminderdata WHERE eventnumber = {$EventID};");
-            ((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . conferdata));
-            $retval2 = mysqli_query( $conn ,  $sql2);
-
-            $message = "Dear User,
-
-You have opted to be reminded for {$EventName}.
-
-The event has been modified by an administrator. It now is represented by the following information.
-
-Name : {$EditName}
-
-Date : {$editdisplaydate}
-
-Presenter : {$editspeakername}
-
-Description :
-{$EditDesc}
-
-Location : {$EditLocation}
-
-We hope this does not cause any incovenience.";
-
-            $mail = new PHPMailer;
-            $mail->isSMTP();
-            $mail->Host = 'smtp.gmail.com';
-            $mail->Port = 587;
-            $mail->SMTPSecure = 'tls';
-            $mail->SMTPAuth = true;
-            $mail->Username = "donotreplyconfer@gmail.com";
-            $mail->Password = "Chirag25";
-
-            // Email Sending Details
-            while($row2 = mysqli_fetch_array($retval2)){
-              $UID = $row2['usernumber'];
-
-              $sql3 = ("SELECT email FROM userdata WHERE usernumber = {$UID};");
-              ((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE " . conferdata));
-              $retval3 = mysqli_query( $conn ,  $sql3);
-              $query3 = mysqli_fetch_array($retval3);
-              $mail->AddBCC($query3);
-            }
-            $mail->Subject = "Changes to {$EventName}";
-            $mail->isHTML(false);
-            $mail->Body = $message;
-
-            $mail->send();
-          }
-        }
     }
     ?>
     <br><br><br><br><br><br><!-- This is for readability on a computer, don't get rid of it. -->
