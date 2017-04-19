d0dab92733483df284014e81abf7bd4a460ade09
Emizaquel <emizaquel@gmail.com>
2017-04-19 19:07:23 +0800

    Help form, settings test 6

--- scripts.js
+++ scripts.js
@@ -1,29 +1,29 @@
 var autoSizeText;
 
 autoSizeText = function() {
-  setInterval(function() {
+  var el1 = document.getElementById('content');
+  var style1 = window.getComputedStyle(el1, null).getPropertyValue('height');
+  var contentHeight = parseFloat(style1);
+
+  var el2 = document.getElementById('title-content');
+  var style2 = window.getComputedStyle(el2, null).getPropertyValue('height');
+  var containerHeight = parseFloat(style2);
+
+  var fontSize = 200;
+  var newFont = fontSize - 1;
+
+  while(contentHeight > containerHeight) {
+  	el.style.fontSize = (newFont) + 'px';
+    var newFont = newFont - 1;
+
     var el1 = document.getElementById('content');
     var style1 = window.getComputedStyle(el1, null).getPropertyValue('height');
     var contentHeight = parseFloat(style1);
-
-    var el2 = document.getElementById('title-content');
-    var style2 = window.getComputedStyle(el2, null).getPropertyValue('height');
-    var containerHeight = parseFloat(style2);
-
-    var fontSize = 200;
-    var newFont = fontSize - 1;
-
-    while(contentHeight > containerHeight) {
-    	el.style.fontSize = (newFont) + 'px';
-      var newFont = newFont - 1;
-
-      var el1 = document.getElementById('content');
-      var style1 = window.getComputedStyle(el1, null).getPropertyValue('height');
-      var contentHeight = parseFloat(style1);
-    }
-  }, 10 * 1000); // 60 * 1000 milsec
+  }
 };
 
 $(document).ready(function() {
+  window.setInterval(function(){
   return autoSizeText();
+}, 5000);
 });
