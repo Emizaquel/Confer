c8a812cef6a3d217f69a6d931df00a26caa4b445
Emizaquel <emizaquel@gmail.com>
2017-04-19 19:17:54 +0800

    Help form, settings test 6

--- scripts.js
+++ scripts.js
@@ -22,7 +22,7 @@ autoSizeText = function() {
     var style1 = window.getComputedStyle(el1, null).getPropertyValue('height');
     var contentHeight = parseFloat(style1);
   }
-  setTimeout(refreshData, 5000);
+  setTimeout(autoSizeText, 5000);
 };
 
 $(document).ready(function() {
