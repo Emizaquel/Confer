a7a7b9e1b25292b9134222dac68897f81076ebf0
Emizaquel <emizaquel@gmail.com>
2017-04-20 00:06:47 +0800

    search bar!

--- Admin-A.php
+++ Admin-A.php
@@ -86,10 +86,12 @@
       </span>
     </a>
     <br>
-    <form method="POST" action="">
-      <input type="text" placeholder="Search Here" name="searchtext" style="height: 45px;width: 65%;font-size: 35px;margin: 5px;">
-      <input type = "submit" name = "search" value = "Submit" style="height: 45px;width: 25%;font-size: 35px;margin: 5px;">
-    </form>
+    <span id="NewEvent">
+      <form method="POST" action="">
+        <input type="text" placeholder="Search Here" name="searchtext" style="height: 45px;width: 65%;font-size: 35px;margin: 5px;">
+        <input type = "submit" name = "search" value = "Submit" style="height: 45px;width: 25%;font-size: 35px;margin: 5px;">
+      </form>
+    </span>
     <?php
     $dbserver = "127.0.0.1:51097";
     $dbuser = "azure";
