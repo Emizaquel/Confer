cde4d420e9fe07195930ca458d9b3d3cc7732427
Emizaquel <emizaquel@gmail.com>
2017-04-20 00:14:21 +0800

    search bar!

--- Admin-A.php
+++ Admin-A.php
@@ -88,8 +88,8 @@
     <br>
     <span style="display: block;background-color: white;border-radius: 15px;border-style: solid;border-color: grey;border-width: 10px;color: black;overflow-x: hidden;min-height: 36px;padding: 10px;text-align:center;">
       <form method="POST" action="">
-        <input type="text" placeholder="Search Here" name="searchtext" style="height: 45px;width: 65%;font-size: 35px;margin: 5px;">
-        <input type = "submit" name = "search" value = "Go" style="height: 45px;width: 15%;font-size: 35px;margin: 5px;">
+        <input type="text" placeholder="Search Here" name="searchtext" style="height: 45px;width: 65%;font-size: 35px;">
+        <input type = "submit" name = "search" value = "Go" style="height: 45px;width: 15%;font-size: 35px;">
       </form>
     </span>
     <br><br>
