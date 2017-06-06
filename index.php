fd98425f5b97a0d0bdfaae5200162bd2c1edd178
Emizaquel <emizaquel@gmail.com>
2017-06-06 11:22:55 +0100

    Attempt to fix socket issues #2

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <head>
+  <?php
+  $dbserver = "127.0.0.1";
+  $port = "51097";
+  $dbuser = "azure";
+  $dbpass = "6#vWHD_$";
+  $dbname = "localdb";
+
+  $conn = ($GLOBALS["___mysqli_ston"] = mysqli_connect($dbserver,  $dbuser,  $dbpass, $port));
+
+  mysqli_close($conn);
+  ?>
+
 <meta http-equiv="refresh" content="0; url=Login.php" />
 <link rel="icon" type="image/png" href="icon.ico">
 <link rel="stylesheet" type="text/css" href="general.css">
 </head>
-
-<?php
-$dbserver = "127.0.0.1";
-$port = "51097";
-$dbuser = "azure";
-$dbpass = "6#vWHD_$";
-$dbname = "localdb";
-
-$conn = ($GLOBALS["___mysqli_ston"] = mysqli_connect($dbserver,  $dbuser,  $dbpass, $port));
-
-mysqli_close($conn);
-?>
