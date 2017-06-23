bfd03a43c4a81af6889a2c49accbb9092a25b255
Emizaquel <emizaquel@gmail.com>
2017-06-23 07:35:52 +0100

    trying to update baseconnect

--- baseconnect.php
+++ baseconnect.php
@@ -6,5 +6,5 @@ $dbuser = "azure";
 $dbpass = "6#vWHD_$";
 $dbname = "localdb";
 
-$conn = mysqli_connect("p:".$dbserver,  $dbuser,  $dbpass, $port);
+$conn = mysqli_connect("p:127.0.0.1",  "azure",  "6#vWHD_$", "49477");
 ?>
