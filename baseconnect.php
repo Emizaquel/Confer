46e5d32b1158991b4e641136069969763aff776a
Emizaquel <emizaquel@gmail.com>
2017-06-23 07:32:06 +0100

    trying to update baseconnect

--- baseconnect.php
+++ baseconnect.php
@@ -6,9 +6,5 @@ $dbuser = "azure";
 $dbpass = "6#vWHD_$";
 $dbname = "localdb";
 
-$conn = ($GLOBALS["___mysqli_ston"] = mysqli_connect("p:".$dbserver,  $dbuser,  $dbpass, $port));
-
-if(! $conn ) {
-  echo ('Could not connect: ' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
-}
+$conn = mysqli_connect("p:".$dbserver,  $dbuser,  $dbpass, $port);
 ?>
