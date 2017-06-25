0b4299feeeb199ecbc776e59a268a655cba58d7d
Emizaquel <zcabheg@ucl.ac.uk>
2017-06-26 03:00:43 +0530

    just updating the port

--- baseconnect.php
+++ baseconnect.php
@@ -1,12 +1,12 @@
 <?php
 
 $dbserver = "127.0.0.1";
-$port = "49477";
+$port = "52090";
 $dbuser = "azure";
 $dbpass = "6#vWHD_$";
 $dbname = "localdb";
 
-$conn = ($GLOBALS["___mysqli_ston"] = mysqli_connect('p:127.0.0.1', 'azure', '6#vWHD_$', 'localdb', 49477));
+$conn = ($GLOBALS["___mysqli_ston"] = mysqli_connect('p:127.0.0.1', 'azure', '6#vWHD_$', 'localdb', 52090));
 
 if(! $conn ) {
   echo ('Could not connect: ' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
